efa072398331b25b2d0251da17e850767e63d6e1
Chandra Dahal <CDahal@hchb.com>
2017-10-18 17:12:13 -0400

    Introducing single page Nav with jquery

--- js/chandra.js
+++ js/chandra.js
@@ -1,14 +1,30 @@
 // HTML document is loaded. DOM is ready.
 $(function() {
 
-// Parallax
-$('.intro-section').parallax({
-imageSrc: 'img/2-2.jpg',
-speed: 0.2
-});
+  // Parallax
+  $('.intro-section').parallax({
+  imageSrc: 'img/2-2.jpg',
+  speed: 0.2
+  });
 
-$('.services-section').parallax({
-imageSrc: 'img/2-3.jpg',
-speed: 0.2
-});
+  $('.services-section').parallax({
+  imageSrc: 'img/2-3.jpg',
+  speed: 0.2
+  });
+
+  //Single Page Navigation
+  $('.single-page-nav').singlePageNav({
+    offset: $('.single-page-nav').outerHeight(),
+    speed: 2000,
+    filter: ':not(.external)',
+    updateHash: true
+  });
+  
+  $('.navbar-toggle').click(function(){
+    $('.single-page-nav').toggleClass('show');
+  });
+
+  $('.single-page-nav a').click(function(){
+    $('.single-page-nav').removeClass('show');
+  });
 });
