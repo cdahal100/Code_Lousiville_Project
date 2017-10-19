81066ae838d1e48f8fc4015ab8f3dd0fc5e845cd
Chandra Dahal <cdahal100@gmail.com>
2017-10-18 22:48:52 -0400

    ScrolluP feature using jquery

--- js/chandra.js
+++ js/chandra.js
@@ -17,6 +17,35 @@ $(function() {
   speed: 0.2
   });
 
+  // jQuery Scroll Up / Back To Top Image
+  $.scrollUp({
+        scrollName: 'scrollUp',      // Element ID
+    scrollDistance: 300,         // Distance from top/bottom before showing element (px)
+    scrollFrom: 'top',           // 'top' or 'bottom'
+    scrollSpeed: 1000,            // Speed back to top (ms)
+    easingType: 'linear',        // Scroll to top easing (see http://easings.net/)
+    animation: 'fade',           // Fade, slide, none
+    animationSpeed: 300,         // Animation speed (ms)
+    scrollText: '', // Text for element, can contain HTML
+    scrollImg: true            // Set true to use image
+    });
+
+  // ScrollUp Placement
+  $( window ).on( 'scroll', function() {
+
+    // If the height of the document less the height of the document is the same as the
+    // distance the window has scrolled from the top...
+    if ( $( document ).height() - $( window ).height() === $( window ).scrollTop() ) {
+
+        // Adjust the scrollUp image so that it's a few pixels above the footer
+        $('#scrollUp').css( 'bottom', '80px' );
+
+    } else {
+        // Otherwise, leave set it to its default value.
+        $('#scrollUp').css( 'bottom', '30px' );
+    }
+  });
+
   //Single Page Navigation
   $('.single-page-nav').singlePageNav({
     offset: $('.single-page-nav').outerHeight(),
